97f5d5521fedee7f1df8e2b357c5a6a0b4ba237c
crystalneko <minecrafthetun@outlook.com>
2025-02-01 11:50:15 +0800

    再次优化

--- src/main/java/org/cneko/justarod/entity/ai/SexualIntercourseGoal.kt
+++ src/main/java/org/cneko/justarod/entity/ai/SexualIntercourseGoal.kt
@@ -18,8 +18,8 @@ class SexualIntercourseGoal(private val neko: SeeeeexNekoEntity) : Goal() {
         val world = neko.world
 
         if (world is ServerWorld) {
-            // 大于 2000 个 Neko，不进行操作
-            if (NekoQuery.NekoData.getNekoCount() > 2000) {
+            // 大于 700 个 Neko，不进行操作
+            if (NekoQuery.NekoData.getNekoCount() > 700) {
                 target = null
                 return
             }

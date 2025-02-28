a7b9652bf13e49706f9e6bf730db9bb474053de2
crystalneko <minecrafthetun@outlook.com>
2025-03-01 00:10:03 +0800

    添加生长剂~

--- src/main/java/org/cneko/justarod/client/renderer/RodRenderer.kt
+++ src/main/java/org/cneko/justarod/client/renderer/RodRenderer.kt
@@ -1,13 +1,48 @@
 package org.cneko.justarod.client.renderer
 
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
 import org.cneko.justarod.entity.RodEntity
 import software.bernie.geckolib.model.GeoModel
 import software.bernie.geckolib.renderer.GeoEntityRenderer
 import org.cneko.justarod.JRUtil.Companion.rodId
-class RodRenderer(renderManager: EntityRendererFactory.Context?): GeoEntityRenderer<RodEntity>(renderManager,RodModel()) {
+import software.bernie.geckolib.cache.`object`.BakedGeoModel
 
+class RodRenderer(renderManager: EntityRendererFactory.Context?): GeoEntityRenderer<RodEntity>(renderManager,RodModel()) {
+    override fun preRender(
+        poseStack: MatrixStack?,
+        animatable: RodEntity?,
+        model: BakedGeoModel?,
+        bufferSource: VertexConsumerProvider?,
+        buffer: VertexConsumer?,
+        isReRender: Boolean,
+        partialTick: Float,
+        packedLight: Int,
+        packedOverlay: Int,
+        colour: Int
+    ) {
+
+        if (animatable?.isBaby == true){
+            poseStack?.scale(0.5f,0.5f,0.5f)
+        }
+        super.preRender(
+            poseStack,
+            animatable,
+            model,
+            bufferSource,
+            buffer,
+            isReRender,
+            partialTick,
+            packedLight,
+            packedOverlay,
+            colour
+        )
+
+    }
 
 }
 class RodModel : GeoModel<RodEntity>() {
@@ -22,4 +57,5 @@ class RodModel : GeoModel<RodEntity>() {
     override fun getAnimationResource(animatable: RodEntity): Identifier {
         return rodId("animations/entity/rod.animation.json")
     }
+
 }
